6562c651835ce5ea79f9561c87609ea9e7018ef7
AShvab <shvabovska@gmail.com>
2023-05-15 00:08:22 +0300

    add searchBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import SearchBar from './Searchbar';
 
 
 class App extends Component {
+  state = {
+    pictureName: '',
+  };
+
+  handleFormSubmit = pictureName => {
+    this.setState({ pictureName});
+   };
   render() {
+
     return (
       <div>
-        <h1>Hello World</h1>
+              {/* onSubmit - передача аргумента в функцію */}
+        <SearchBar onSubmit={this.handleFormSubmit}/>
+        <ToastContainer autoClose={2000} position="top-center" theme="colored"/>
       </div>
     );
-  }    
-}
+  }
+} 
 
 export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ImSearch } from 'react-icons/im';
+
+export default class SearchBar extends Component {
+  state = {
+    pictureName: '',
+  };
+
+  handleSearchChange = event => {
+    this.setState({ pictureName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.pictureName.trim() === '') {
+      return toast.warn('Enter picture name');
+    }
+    // При сабміті форми викликаємо метод з Арр onSubmit та передаємо йому значення state з цієї форми
+    this.props.onSubmit(this.state.pictureName);
+    this.setState({ pictureName: '' });
+  };
+
+  render() {
+    return (
+      <header className="searchBar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <ImSearch  />
+          </button>
+
+          <input
+            onChange={this.handleSearchChange}
+            value={this.state.pictureName}
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
