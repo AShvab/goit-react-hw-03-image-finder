e496500ac1eba524311112fb6c188dd31f9cf4a3
AShvab <shvabovska@gmail.com>
2023-05-16 21:50:09 +0300

    fixed

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const ButtonLoad = styled.button`
-width: 100px;
+  width: 100px;
   padding: 8px 16px;
   margin: 0 auto;
   border-radius: 2px;
@@ -21,9 +21,8 @@ width: 100px;
   min-width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-    &:hover,
-    &:focus {
-        background-color: #303f9f;
-    }
+  &:hover,
+  &:focus {
+    background-color: #303f9f;
+  }
 `;
-
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,104 +1,3 @@
-// import PropTypes from 'prop-types';
-// import { Component } from 'react';
-// import { Button } from '../Button/Button';
-// import { Loader } from '../Loader/Loader';
-// import { fetchImages } from '../../services/api';
-// import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
-// import { toast } from 'react-toastify';
-// import { Gallery } from './ImageGallery.styled';
-
-// export class ImageGallery extends Component {
-//   state = {
-//     gallery: [],
-//     error: null,
-//     status: 'idle',
-//     page: 1,
-//   };
-
-//   async componentDidUpdate(prevProps, _) {
-//     const prevSearchQuery = prevProps.pictureName;
-//     const nextSearchQuery = this.props.pictureName;
-
-//     if (prevSearchQuery !== nextSearchQuery) {
-//       this.setState({ status: 'pending', page: 1 });
-
-//       try {
-//         const { hits, total } = await fetchImages(nextSearchQuery, 1);
-//         // ==========---------
-//         if (total === 0) {
-//           toast.error('Sorry, we found no images');
-//           return; 
-//         }      
-      
-//         this.setState(prevState => {
-//           return {
-//             gallery: hits,
-//             status: 'resolved',
-//             page: prevState.page + 1,
-//           };
-//         });
-//       } catch (error) {
-//         this.setState({ error, status: 'rejected' });
-//       }
-//     }
-//   }
-
-//   loadMoreHandler = async () => {
-//     try {
-//       const { hits } = await fetchImages(
-//         this.props.pictureName,
-//         this.state.page
-//       );
-//       this.setState(prevState => {
-//         return {
-//           gallery: [...prevState.gallery, ...hits],
-//           page: prevState.page + 1,
-//         };
-//       });
-//     } catch (error) {
-//       this.setState({ error, status: 'rejected' });
-//     }
-//   };
-
-//   render() {
-//     const { gallery, error, status } = this.state;
-
-//     if (status === 'pending') {
-//       return <Loader />;
-//     }
-
-//     if (status === 'rejected') {
-//       toast.error(`Error: ${error.message}`);
-//       return;
-//     }
-
-//     if (status === 'resolved') {
-//       return (
-//         <>
-//           <Gallery>
-//             {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
-//               <ImageGalleryItem
-//                 key={id}
-//                 url={webformatURL}
-//                 alt={tags}
-//                 largeImage={largeImageURL}
-//               />
-//             ))}
-//           </Gallery>
-//           <Button onClick={this.loadMoreHandler}>Load more</Button>
-//         </>
-//       );
-//     }
-//   }
-// }
-
-// ImageGallery.propTypes = {
-//   pictureName: PropTypes.string.isRequired,
-// };
-
-// export default ImageGallery;
-
-
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { Button } from '../Button/Button';
@@ -127,8 +26,7 @@ export class ImageGallery extends Component {
         const { hits, total } = await fetchImages(nextSearchQuery, 1);
 
         if (total === 0) {
-          toast.error('Sorry, we found no images');
-          this.setState({ error: new Error('No images found'), status: 'rejected' });
+          this.setState({ error: new Error('Sorry, we found no images'), status: 'rejected' });
           return;
         }
 
@@ -159,6 +57,7 @@ export class ImageGallery extends Component {
       });
     } catch (error) {
       this.setState({ error, status: 'rejected' });
+      return;
     }
   };
 
